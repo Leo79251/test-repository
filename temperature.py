190d310caf651fe416a581f7b9139df8f1688150
Leo79251 <yicigu41@gmail.com>
2026-06-08 13:37:09 +1200

    Add files via upload

--- temperature.py
+++ temperature.py
@@ -5,4 +5,15 @@ class TemperatureConverter:
     
     def convert_fahrenheit_to_celsius(fahrenheit_str):
         '''Convert Fahrenheit to Celsius'''
+        try:
+            fahrenheit = float(fahrenheit_str)
+        except ValueError:
+            return "Please enter a number", "error"  # Return results and status
+
+        if fahrenheit < TemperatureConverter.ABSOLUTE_ZERO_F:
+            return "Temperature too low", "error"
+
+        celsius = (fahrenheit - 32) * 5 / 9
+        return f"{celsius:.2f} °C", "success"
+
         
