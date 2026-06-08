df2af4005a3671c62ddf681d4aa55ac4de3f4607
Leo79251 <yicigu41@gmail.com>
2026-06-08 13:36:18 +1200

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,8 @@
+class TemperatureConverter:
+    '''Responsible for verifying temperature values ​​and converting between Celsius and Fahrenheit'''
+    ABS_ZERO_F = -459.67
+    ABS_ZERO_C = -273.15
+    
+    def convert_fahrenheit_to_celsius(fahrenheit_str):
+        '''Convert Fahrenheit to Celsius'''
+        
