37dd25143ea17ea21cffe9d3f8220d848c6f5c7c
Leo79251 <yicigu41@gmail.com>
2026-06-09 15:18:33 +1200

    Add files via upload

--- temperature.py
+++ temperature.py
@@ -15,5 +15,33 @@ class TemperatureConverter:
 
         celsius = (fahrenheit - 32) * 5 / 9
         return f"{celsius:.2f} °C", "success"
+    
+    def convert_celsius_to_fahrenheit(celsius_str):
+        '''Convert Celsius to Fahrenheit'''
+        try:
+            celsius = float(celsius_str)
+        except ValueError:
+            return "Please enter a number", "error"
+
+        if celsius < TemperatureConverter.ABSOLUTE_ZERO_C:
+            return "Temperature too low", "error"
+
+        fahrenheit = (celsius * 9 / 5) + 32
+        return f"{fahrenheit:.2f} °F", "success" 
+    
+    
+import tkinter as tk
+from tkinter import ttk
+from TemperatureConverter import TemperatureConverter
+
+class ConverterGUI:
+    '''Temperature converter graphical user interface class'''
+    def __init__(self, root):
+        
+            self.root = root
+            self.root.title("Temperature Converter")
+            self.root.geometry("500x400")    
+
+            
 
         
